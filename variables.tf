b05a8d2b29cacd62b19467db44d7e150d322c84c
User <user@Student.local>
2020-01-22 16:36:24 +0900

    second commit-4

--- variables.tf
+++ variables.tf
@@ -1,8 +1,8 @@
 
 variable "access_key" {}
 variable "secret_key" {}
-variable "region" {ap-northeast-1}
-variable "ami" {ami-06d9ad3f86032262d}
+variable "region" {default = "ap-northeast-1"}
+variable "ami" {default = "ami-06d9ad3f86032262d"}
 variable "hello_tf_instance_count" {
     default = 2
 }
